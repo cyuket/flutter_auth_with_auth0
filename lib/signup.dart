16481c369c9b610ad9a79155b54eb06d559c505d
cyuket <cyriluket12@gmail.com>
2020-07-19 00:48:08 +0100

    Signup page Completed

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,5 +1,7 @@
 import 'package:auth0/login.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_auth0/flutter_auth0.dart';
+import 'constant.dart';
 
 class Signup extends StatefulWidget {
   @override
@@ -7,13 +9,58 @@ class Signup extends StatefulWidget {
 }
 
 class _SignupState extends State<Signup> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   final emailController = TextEditingController();
 
   final passwordController = TextEditingController();
 
+  Auth0 auth;
+  bool busy = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Auth0 auth = Auth0(baseUrl: 'https://$domain/', clientId: clientId);
+  }
+
+  signup({String email, String password}) async {
+    try {
+      setState(() {
+        busy = true;
+      });
+      await auth.auth.createUser({
+        'email': email,
+        'password': password,
+        'connection': 'Username-Password-Authentication'
+      });
+      setState(() {
+        busy = false;
+      });
+      _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.green,
+          content: new Text("$email has successfully logged in"),
+        ),
+      );
+    } catch (e) {
+      setState(() {
+        busy = false;
+      });
+      _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.red,
+          content: new Text(e),
+        ),
+      );
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text("Signup"),
       ),
@@ -66,7 +113,12 @@ class _SignupState extends State<Signup> {
               ),
               SizedBox(height: 20),
               InkWell(
-                onTap: null,
+                onTap: () async {
+                  await signup(
+                    email: emailController.text,
+                    password: passwordController.text,
+                  );
+                },
                 child: Container(
                   height: 40,
                   decoration: BoxDecoration(
@@ -74,14 +126,20 @@ class _SignupState extends State<Signup> {
                     color: Colors.blue,
                   ),
                   child: Center(
-                    child: Text(
-                      "Signup",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+                    child: !busy
+                        ? Text(
+                            "Signup",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        : CircularProgressIndicator(
+                            strokeWidth: 2,
+                            valueColor:
+                                AlwaysStoppedAnimation<Color>(Colors.white),
+                          ),
                   ),
                 ),
               ),
