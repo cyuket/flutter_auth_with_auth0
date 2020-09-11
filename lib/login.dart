ed77f9587dc5713959688de5f54f1f88dafa48a2
cyuket <cyriluket12@gmail.com>
2020-09-11 12:29:22 +0100

    Login Page

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,7 @@
+import 'package:auth0/constant.dart';
 import 'package:auth0/signup.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_auth0/flutter_auth0.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -7,9 +9,53 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
   final emailController = TextEditingController();
 
   final passwordController = TextEditingController();
+  Auth0 auth;
+  bool busy = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Auth0 auth = Auth0(baseUrl: 'https://$domain/', clientId: clientId);
+  }
+
+  login({String email, String password}) async {
+    try {
+      setState(() {
+        busy = true;
+      });
+      await auth.auth.passwordRealm({
+        'username': email,
+        'password': password,
+        'realm': 'Username-Password-Authentication'
+      });
+      setState(() {
+        busy = false;
+      });
+      _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.green,
+          content: new Text("$email has successfully logged in"),
+        ),
+      );
+    } catch (e) {
+      setState(() {
+        busy = false;
+      });
+      _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.red,
+          content: new Text(e),
+        ),
+      );
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -66,7 +112,12 @@ class _LoginState extends State<Login> {
               ),
               SizedBox(height: 20),
               InkWell(
-                onTap: null,
+                onTap: () async {
+                  await login(
+                    email: emailController.text,
+                    password: passwordController.text,
+                  );
+                },
                 child: Container(
                   height: 40,
                   decoration: BoxDecoration(
@@ -74,14 +125,20 @@ class _LoginState extends State<Login> {
                     color: Colors.blue,
                   ),
                   child: Center(
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+                    child: !busy
+                        ? Text(
+                            "Login",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        : CircularProgressIndicator(
+                            strokeWidth: 2,
+                            valueColor:
+                                AlwaysStoppedAnimation<Color>(Colors.white),
+                          ),
                   ),
                 ),
               ),
